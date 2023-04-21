d0e73a2b62af3d5c8cde405036f46e535061854e
Incbom <99501939+IncbomDev@users.noreply.github.com>
2023-04-21 10:05:27 -0400

    This is where the bug is
    Creating a bookmark where the bug is

--- src/main/java/me/craftystudios/milestones/Guis/GuiUtils.java
+++ src/main/java/me/craftystudios/milestones/Guis/GuiUtils.java
@@ -3,7 +3,7 @@ package me.craftystudios.milestones.Guis;
 import net.wesjd.anvilgui.AnvilGUI;
 
 public class GuiUtils {
-
+//creating commit message for identifying bug quickly
     public String milestoneNameInput(){
         new AnvilGUI.Builder()
             .onClose( player -> {
